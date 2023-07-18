084551c8c67e08b20dddb217123b8537f5e30027
Muyinda Rogers <muyinda.rodgers@wasoko.com>
2023-07-19 01:53:14 +0300

    Chunks

---
+++ src/main/java/com/kodeinc/authservice/configs/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.kodeinc.authservice.configs;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig   {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+         http
+                 .authorizeRequests()
+                 .anyRequest()
+                 .authenticated();
+
+
+         http.formLogin(null);
+         http.httpBasic(null);
+
+
+
+
+
+         return http.build();
+    }
+}
