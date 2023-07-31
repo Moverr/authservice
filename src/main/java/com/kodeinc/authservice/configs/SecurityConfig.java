589435161ba3311e396ff3a0fe64e265b067bad9
Muyinda Rogers <muyinda.rodgers@wasoko.com>
2023-07-31 20:21:58 +0300

    Authentication Controller.

--- src/main/java/com/kodeinc/authservice/configs/SecurityConfig.java
+++ src/main/java/com/kodeinc/authservice/configs/SecurityConfig.java
@@ -4,8 +4,10 @@ package com.kodeinc.authservice.configs;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -81,6 +83,10 @@ public class SecurityConfig   {
         provider.setUserDetailsService(userDetailsService());
         provider.setPasswordEncoder(passwordEncoder());
         return provider;
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
     @Bean
--- src/main/java/com/kodeinc/authservice/controllers/AuthenticationController.java
+++ src/main/java/com/kodeinc/authservice/controllers/AuthenticationController.java
@@ -1,9 +1,15 @@
 package com.kodeinc.authservice.controllers;
 
+import com.kodeinc.authservice.configs.JwtUtils;
 import com.kodeinc.authservice.models.dtos.requests.AuthenticationRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,16 +21,26 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("api/v1/auth")
+@RequiredArgsConstructor
 public class AuthenticationController {
 
-    private AuthenticationManager authenticationManager;
+
+    private final AuthenticationManager authenticationManager;
+    private final  UserDetailsService userDetailsService;
+    private final JwtUtils jwtUtils;
+
+    @PostMapping("/authenticate")
     public ResponseEntity<String> authenticate(
             @RequestBody AuthenticationRequest request
-    ){
+    ) {
         authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(request.getEmail(),request.getPassword())
+                new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())
         );
+        final UserDetails user = userDetailsService.loadUserByUsername(request.getUsername());
+        if(user != null){
+           return    ResponseEntity.ok(jwtUtils.generateToken(user));
+        }
 
-
+        return ResponseEntity.badRequest().body("Invalid username or password");
     }
 }
