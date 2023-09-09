043ddebe95f1ab94d20595fa28dc3109887fa022
Muyinda Rogers <muyinda.rodgers@wasoko.com>
2023-09-10 01:59:26 +0300

    Debugging, Adding Base controller

--- src/main/java/com/kodeinc/authservice/controllers/AuthenticationController.java
+++ src/main/java/com/kodeinc/authservice/controllers/AuthenticationController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("api/v1/auth")
 @RequiredArgsConstructor
-public class AuthenticationController {
+public class AuthenticationController extends BaseController<String>{
 
 
     @Autowired
---
+++ src/main/java/com/kodeinc/authservice/dtos/responses/ApiResponse.java
@@ -0,0 +1,27 @@
+package com.kodeinc.authservice.dtos.responses;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ * @author  moverr@gmail.com
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class ApiResponse<T> implements Serializable {
+
+    private static final long serialVersionUID = 2606790555796441397L;
+
+    private boolean status;
+    private int successCode;
+    private String successDescription;
+    private int errorCode;
+    private String errorDescription;
+    private T data;
+}
