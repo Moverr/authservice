a8ab41afc625f193b9ec325b626d0cf0e42bafbb
Muyinda Rogers <muyinda.rodgers@wasoko.com>
2023-12-18 05:01:43 +0300

    Debuggging ..

--- src/main/java/com/kodeinc/authservice/models/dtos/responses/PermissionResponse.java
+++ src/main/java/com/kodeinc/authservice/models/dtos/responses/PermissionResponse.java
@@ -1,6 +1,5 @@
 package com.kodeinc.authservice.models.dtos.responses;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.kodeinc.authservice.models.entities.entityenums.PermissionLevelEnum;
